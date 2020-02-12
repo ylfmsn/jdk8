f1e4853870434fe8c819026091ae86e7c4c41758
ylfmsn <yuelifeng0415@aliyun.com>
2020-02-12 17:40:12 +0800

    update

--- src/main/java/com/ylf/jdk8/PredicateTest2.java
+++ src/main/java/com/ylf/jdk8/PredicateTest2.java
@@ -8,7 +8,7 @@ public class PredicateTest2 {
 
     public static void main(String[] args) {
 
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> list = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         PredicateTest2 predicateTest2 = new PredicateTest2();
         // 奇数
@@ -28,12 +28,41 @@ public class PredicateTest2 {
         System.out.println("----------------");
         // 一个都不打印
         predicateTest2.conditionFilter(list, item -> false);
+        System.out.println("----------------");
 
+        // 找出大于5且为偶数
+        predicateTest2.conditionFilter2(list, item -> item > 5, item -> item % 2 == 0);
+        System.out.println("----------------");
+        // 找出所有的偶数，即奇数
+        predicateTest2.conditionFilter3(list, item -> item % 2 != 0);
+        System.out.println("----------------");
     }
 
     public void conditionFilter(List<Integer> list, Predicate<Integer> predicate) {
         for (Integer integer : list) {
             if (predicate.test(integer)) {
+                System.out.println(integer);
+            }
+        }
+    }
+
+    /**
+     * and
+     */
+    public void conditionFilter2(List<Integer> list, Predicate<Integer> predicate1, Predicate<Integer> predicate2) {
+        for (Integer integer : list) {
+            if (predicate1.and(predicate2).test(integer)) {
+                System.out.println(integer);
+            }
+        }
+    }
+
+    /**
+     * negate
+     */
+    public void conditionFilter3(List<Integer> list, Predicate<Integer> predicate) {
+        for (Integer integer : list) {
+            if (predicate.negate().test(integer)) {
                 System.out.println(integer);
             }
         }
